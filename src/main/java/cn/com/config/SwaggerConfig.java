e2746b961a6513d4ad54d3c0e5e0e9c95008c77a
bai-jia-jian <344713865@qq.com>
2020-05-02 18:31:59 +0800

    111

---
+++ src/main/java/cn/com/config/SwaggerConfig.java
@@ -0,0 +1,17 @@
+package cn.com.config;
+
+
+/*
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig extends WebMvcConfigurationSupport{
+    @Bean
+    public Docket createRestApi(){
+        return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class)).build();
+
+    }
+
+
+}
+*/
